8a783f130b1bfd5ce46990fc2fefbc7f9bada340
JBowman <L00170244@student.lyit.ie>
2021-10-07 22:40:55 +0100

    Adding empty file for today's work

---
+++ FollowAlong.py
@@ -0,0 +1,12 @@
+#
+# File        : FollowAlong.py  
+# Created     : 07/10/2021
+# Author      : James Bowman
+# Version     : v1.0.0
+# Licensing   : (C) James Bowman
+#               Available under GNU Public License (GPL)
+# Description : Following along with today's course notes
+#
+
+if __name__ == '__main__':
+    pass
